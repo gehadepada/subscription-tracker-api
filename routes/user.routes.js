d81a326eff04323dc372f85563e0aa438fd0d3c8
gehadepada <gehadepada@gmail.com>
2026-01-01 21:02:44 +0200

    feat:add userRoute

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import {Route} from "express";
+
+const userRoute = Router();
+
+userRoute.get('/',(req,res )=>res.send( {title:"GET allusers"}));
+
+userRoute.get('/:id',(req,res )=>res.send( {title:"GET user details"}));
+
+userRoute.post('/',(req,res )=>res.send( {title:"CREATE new user"}));
+
+userRoute.put('/:id',(req,res )=>res.send( {title:"UPDATE user details"}));
+
+userRoute.delete('/:id',(req,res )=>res.send( {title:"DELETE user"}));
+
+export default userRoute;
