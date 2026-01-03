f2704ca7d4a3bb197c628c20a301bda3e7ddf7be
gehadepada <gehadepada@gmail.com>
2026-01-03 04:24:07 +0200

    feat:add user model

---
+++ models/subscription.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const userScheme = new mongoose.Schema({
+    name:{
+        type : String , 
+        required : [true , "Name is required"],
+        tirm : true , 
+    },
+    email:{
+        type : String,
+        required : [true , "Email is required"],
+        lowercase:true,
+    },
+    password :{
+        type:String,
+        required :[true , "Password is required"],
+        minLength : 6,
+    }
+},
+{timestamps:true});
+const User = mongoose.model('User' , userScheme);
+export default User;
