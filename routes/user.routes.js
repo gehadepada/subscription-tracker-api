5e512f503e2432ee587f5acc83050086c21f8059
gehadepada <gehadepada@gmail.com>
2026-01-06 21:47:42 +0200

    feat:add auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
+import { getUser, getUsers } from "../controllers/user.controller";
+import authorize from "../middlewares/auth.middleware";
 
 const userRoute = Router();
 
-userRoute.get('/',(req,res )=>res.send( {title:"GET allusers"}));
+userRoute.get('/',getUsers);
 
-userRoute.get('/:id',(req,res )=>res.send( {title:"GET user details"}));
+userRoute.get('/:id',authorize,getUser);
 
 userRoute.post('/',(req,res )=>res.send( {title:"CREATE new user"}));
 
