f53e32afa0339d01d2563d79fc32cb6efa4f9d6d
gehadepada <gehadepada@gmail.com>
2026-01-06 17:37:53 +0200

    feat:add auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,100 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import { JWT_SECRET } from "../config/env.js";
+import { JWT_EXPIRED_IN } from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists) {
+     const error=new Error("User already exist");
+     error.statusCode=409;
+     throw error;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign(
+      { userId: newUsers[0]._id },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        user: newUsers[0],
+        token,
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+     const error=new Error("User not found");
+     error.statusCode=404;
+     throw error;
+     
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+     const error=new Error("Invalid password");
+     error.statusCode=401;
+     throw error;
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: {
+        user,
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
